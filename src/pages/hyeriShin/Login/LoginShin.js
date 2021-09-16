ae3722e21eb63a76bb910bdfedcaeca43384cd3b
Jetom88 <jetom.shin@gmail.com>
2021-09-16 17:20:39 +0900

    Add: Lobster font

--- src/pages/hyeriShin/Login/LoginShin.js
+++ src/pages/hyeriShin/Login/LoginShin.js
@@ -1,8 +1,51 @@
 import React, { Component } from 'react';
-//import { withRouter } from 'react-router';
+import './LoginShin.scss';
+
 class LoginShin extends Component {
+  goToMainShin = () => {
+    this.props.history.push('/MainShin');
+  };
+
   render() {
-    return null;
+    return (
+      <div className="loginBody">
+        <div className="cotainer">
+          <header>
+            <h1 className="logo">
+              Tomstagram <i className="fas fa-dog"></i>
+            </h1>
+          </header>
+
+          <section>
+            <article>
+              <div className="loginBox">
+                <form>
+                  <input
+                    type="text"
+                    placeholder="전화번호, 사용자 이름 또는 이메일"
+                    className="loginInput info"
+                  />
+                  <input
+                    type="password"
+                    placeholder="비밀번호"
+                    className="loginInput password"
+                  />
+
+                  <button className="loginBtn" onClick={this.goToMainShin}>
+                    로그인
+                  </button>
+                </form>
+              </div>
+            </article>
+          </section>
+
+          <footer>
+            {/* <a href="#" target="_blank" class="forget_password">비밀번호를 잊으셨나요?</a> */}
+          </footer>
+        </div>
+      </div>
+    );
   }
 }
+
 export default LoginShin;
