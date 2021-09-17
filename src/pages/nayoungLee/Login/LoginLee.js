6fa9cd92884f4c93f73c9c3f2618da377792e6a0
nylee9621 <nylee9621>
2021-09-17 17:51:43 +0900

    Add: Mission2 - 로그인 버튼 활성화 기능 구

--- src/pages/nayoungLee/Login/LoginLee.js
+++ src/pages/nayoungLee/Login/LoginLee.js
@@ -8,18 +8,48 @@ class LoginLee extends React.Component {
     this.state = {
       id: '',
       pw: '',
+      isIdValidation: false,
+      isPwValidation: false,
+      isBtnOn: false,
     };
   }
 
   handleIdInput = e => {
-    this.setState({
-      id: e.target.value,
-    });
+    this.setState(
+      {
+        id: e.target.value,
+        isIdValidation: this.changeIdValidation(e.target.value),
+      },
+      this.toggleBtn
+    );
   };
 
   handlePwInput = e => {
+    this.setState(
+      {
+        pw: e.target.value,
+        isPwValidation: this.changePwValidation(e.target.value),
+      },
+      this.toggleBtn
+    );
+  };
+
+  changeIdValidation = id => {
+    const regId = /^[a-z]+[a-z0-9]{5,19}$/;
+    const regEmail =
+      /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+    const regPnum = /^01([0|1|6|7|8|9]?)-?([0-9]{3,4})-?([0-9]{4})$/;
+    return regId.test(id) || regEmail.test(id) || regPnum.test(id);
+  };
+
+  changePwValidation = pw => {
+    const regPw = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,16}$/;
+    return regPw.test(pw);
+  };
+
+  toggleBtn = () => {
     this.setState({
-      pw: e.target.value,
+      isBtnOn: this.state.isIdValidation && this.state.isPwValidation,
     });
   };
 
@@ -43,7 +73,11 @@ class LoginLee extends React.Component {
             placeholder="비밀번호"
             onChange={this.handlePwInput}
           />
-          <button className="button" disabled={true} onClick={this.goToMain}>
+          <button
+            className={this.state.isBtnOn ? 'active' : ''}
+            disabled={!this.state.isBtnOn}
+            onClick={this.goToMain}
+          >
             로그인
           </button>
         </div>
